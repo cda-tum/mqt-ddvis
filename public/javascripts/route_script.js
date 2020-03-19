b0fb74d53278743bdf3bc71a92957836ee4f2791
Michael Artner <michael.artner@hotmail.de>
2020-03-19 18:14:21 +0100

    embedded the svg of the qdd directly into html + introduced example-algorithm-accordion + cleaned up debug code

--- public/javascripts/route_script.js
+++ public/javascripts/route_script.js
@@ -1,77 +1,17 @@
 
 const stepWaitTime = 700;   //in ms
 
-//from: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_tabs
-function openTab(event, tabId) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tabId).style.display = "block";
-    event.currentTarget.className += " active";
-}
-
 $(() =>  {
     /* ######################################################### */
-    $('#init').on('click', () => {
-        debugText();
-        
-        $.ajax({
-            url: '/init',
-            contentType: 'application/json',
-            success: (res) => {
-                debugText(res.msg);
-
-                var q_algo = $('#q_algo');
-                q_algo.val(res.algo);
-            }
-        });
-    });
-    /* ######################################################### */
-    $('#addBell').on('click', () => {
-        debugText();
-        
-        $.ajax({
-            url: '/addBell',
-            contentType: 'application/json',
-            success: (res) => {
-                debugText(res.msg);
-                
-                var q_algo = $('#q_algo');
-                q_algo.val(res.algo);
-            }
-        });
-    });
-    /* ######################################################### */
-    $('#print').on('click', () => {
-        var op = $('#output');
-        op.text("");
-        
-        $.ajax({
-            url: '/print',
-            contentType: 'application/json',
-            success: (res) => {
-                debugText(res.msg);
-                print(res.ip);
-            }
-        });
-    });
-    
-    /* ######################################################### */
     $('#load').on('click', () => {
-        var op = $('#output');
+        const op = $('#output');
         op.text("");
-        var q_algo = $('#q_algo').val();
+        const q_algo = $('#q_algo').val();
         
         $.post("/load", { algo: q_algo }, 
             (res) => {
                 debugText(res.msg);
-                print(res.ip);
+                print(res.svg);
             }
         );
     });
@@ -86,8 +26,8 @@ $(() =>  {
                 success: (res) => {
                     debugText(res.msg);
                     
-                    if(res.reload === "true") {
-                        print(res.ip);
+                    if(res.svg) {
+                        print(res.svg);
                         setTimeout(() => func(), stepWaitTime); //wait a bit so the current qdd can be shown to the user
                     }
                 }
@@ -104,8 +44,8 @@ $(() =>  {
             contentType: 'application/json',
             success: (res) => {
                 debugText(res.msg);
-                
-                if(res.reload === "true") print(res.ip);
+
+                if(res.svg) print(res.svg);
             }
         });
     });
@@ -118,8 +58,8 @@ $(() =>  {
             contentType: 'application/json',
             success: (res) => {
                 debugText(res.msg);
-                
-                if(res.reload === "true") print(res.ip);
+
+                if(res.svg) print(res.svg);
             }
         });
     });
@@ -134,8 +74,8 @@ $(() =>  {
                 success: (res) => {
                     debugText(res.msg);
                     
-                    if(res.reload === "true") {
-                        print(res.ip);
+                    if(res.svg) {
+                        print(res.svg);
                         setTimeout(() => func(), stepWaitTime); //wait a bit so the current qdd can be shown to the user
                     }
                 }
@@ -145,13 +85,11 @@ $(() =>  {
     });
 });
 
-function ipToFile(ip) {
-    return "./data/" + ip + ".dot.svg";
-}
+function print(svg) {
+    const div = document.getElementById('svg_div');
+    const start = svg.indexOf('<svg');
 
-function print(ip) {
-    const img = $('#qdd_img');
-    img.attr("data", ipToFile(ip));
+    div.innerHTML = svg.substring(start);
 }
 
 function debugText(text = "") {
