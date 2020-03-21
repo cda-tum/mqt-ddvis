f7a8143f9479008f32c249e28ba9d8345854eade
Michael Artner <michael.artner@hotmail.de>
2020-03-21 17:50:46 +0100

    introduced a more flexible and better readable html-structure

---
+++ public/javascripts/main.js
@@ -0,0 +1,20 @@
+
+//from: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_tabs
+function openTab(event, tabId) {
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabId).style.display = "block";
+    event.currentTarget.className += " active";
+}
+
+
+function dragOverHandler(event) {
+    event.preventDefault();
+}
