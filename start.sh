b965a3d92ce9eb0ec72d3e81f33f9701eca47295
Lukas Burgholzer <lukas.burgholzer@jku.at>
2020-03-24 13:31:17 +0100

    Corrected start.sh
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

--- start.sh
+++ start.sh
@@ -8,11 +8,12 @@ if [ ! "$PACKAGES" == "$(cat /tmp/INSTALLED_PACKAGES)" ]; then
   notroot install $PACKAGES
   echo $PACKAGES > /tmp/INSTALLED_PACKAGES
 
-  wget -c https://github.com/Kitware/CMake/releases/download/v3.17.0/cmake-3.17.0-Linux-x86_64.tar.gz -O - | tar -xfz
+  wget https://github.com/Kitware/CMake/releases/download/v3.17.0/cmake-3.17.0-Linux-x86_64.tar.gz
+  tar -xvf cmake-3.17.0-Linux-x86_64.tar.gz
 else
   source /tmp/notroot/bashrc
 fi
-export PATH=$PATH:/tmp/cmake-3.17.0/bin
+export PATH=$PATH:/tmp/cmake-3.17.0-Linux-x86_64/bin
 cd
 pnpm install --reporter silent --prefer-offline --audit false
 npm run-script run --silent
